cebab188a4c765c4690fa7e35a95d629ebdfd949
Gökhan ALKAN <cigalkan@gmail.com>
2015-08-02 23:28:35 +0300

    Update corescanner.py

--- lib/active/corescanner.py
+++ lib/active/corescanner.py
@@ -21,21 +21,15 @@ class CoreScanner(object):
 		self.ip_file = ip_file_to_scan
 		self.__output_file = output_file
 	
-		if nmap_optimize:
-			nmap_options = "{0} {1} -iL {2}".format(CoreScanner.scan_type_options[self.scan_type], Core.nmap_optimize, self.ip_file)
-		else:
-			nmap_options = "{0} -iL {1}".format(CoreScanner.scan_type_options[self.scan_type], self.ip_file)
-
+		nmap_options = "{0} {1} -iL {2}".format(CoreScanner.scan_type_options[self.scan_type], Core.nmap_optimize, self.ip_file) if nmap_optimize else "{0} -iL {1}".format(CoreScanner.scan_type_options[self.scan_type], self.ip_file)
+		
 		self.proc_cmd = "{0} {1}".format(Core.commands_path["nmap"], nmap_options)
 	
 
 	def run(self):
 
 		self.ip_file_to_scan.seek(0)
-		if self.scan_type in ( "PortScan", "ScriptScan"):
-			cmd = "{0} {1} -oA {2}".format(self.proc_cmd, self.scan_options, self.__output_file)
-		else:
-			cmd = "{0} -oA {1}".format(self.proc_cmd, self.__output_file)
-
+		cmd = "{0} {1} -oA {2}".format(self.proc_cmd, self.scan_options, self.__output_file) if self.scan_type in ( "PortScan", "ScriptScan") else "{0} -oA {1}".format(self.proc_cmd, self.__output_file)
+		
 		print "%s : %s"% (self.scan_type, cmd)
 		proc = subprocess.Popen([cmd], shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE,).communicate()
