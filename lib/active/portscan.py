d6dc1fca4af01b7a8f4a3aebd92b519d11e1f7d3
Gökhan ALKAN <cigalkan@gmail.com>
2015-08-06 09:21:05 +0300

    Update portscan.py

--- lib/active/portscan.py
+++ lib/active/portscan.py
@@ -1,8 +1,9 @@
 
 try:
 	import datetime
-	from lib.core.config_parser import ConfigParser
 	from lib.active.corescanner import CoreScanner
+	from lib.core.config_parser import ConfigParser
+	from lib.core.exceptions import FlashLightExceptions
 except ImportError, err:
 	from lib.core.core import Core
 	Core.print_error(err)
@@ -13,7 +14,10 @@ class PortScan(CoreScanner):
     	def __init__(self, config_file, output_dir, ip_file_to_scan, nmap_optimize, scan_type):
 
 		self._ip_file_to_scan = ip_file_to_scan
-		self._scan_options = ConfigParser.get_ports_options(config_file)
+		try:
+			self._scan_options = ConfigParser.get_ports_options(config_file)
+		except Exception, err:
+			raise FlashLightExceptions(str(err))	
 
 		output_file = "{0}{1}-{2}".format(output_dir, scan_type, datetime.datetime.now().strftime("%Y%m%d%H%M"))
 
